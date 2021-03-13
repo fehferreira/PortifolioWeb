4bedbcd742f5505e2d9b4f617c292ceb4daee869
Felipe <felipe.ferreira1003@gmail.com>
2021-03-13 17:22:21 -0300

    Creating project and initials files

---
+++ src/main/java/br/com/pessoal/portifolio/PortifolioWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.pessoal.portifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifolioWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortifolioWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/pessoal/portifolio/PortifolioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pessoal.portifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
