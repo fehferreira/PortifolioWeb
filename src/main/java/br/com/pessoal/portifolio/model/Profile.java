1cabc8b27fd45d779d39cfd17207f41a7259c890
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-03-24 01:08:00 -0300

    Include a dependecy of springframework security and Developed a Profile class entity

--- src/main/java/br/com/pessoal/portifolio/model/Profile.java
+++ src/main/java/br/com/pessoal/portifolio/model/Profile.java
@@ -1,4 +1,34 @@
 package br.com.pessoal.portifolio.model;
 
-public class Profile {
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Profile implements GrantedAuthority{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String type;
+
+    public Profile(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.type;
+    }
 }
