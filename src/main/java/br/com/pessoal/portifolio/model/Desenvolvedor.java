b56e2ff35f327069f9eba523a8b01bed1600e2d5
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-03-24 02:12:49 -0300

    Refact DesenvolvedorForm and include a Constructor and Getters and Setters on Desenvolvedor entity

--- src/main/java/br/com/pessoal/portifolio/model/Desenvolvedor.java
+++ src/main/java/br/com/pessoal/portifolio/model/Desenvolvedor.java
@@ -23,8 +23,84 @@ public class Desenvolvedor {
     @Enumerated(EnumType.STRING)
     private Senioridade devSenioridade;
 
-    @OneToOne
     @Enumerated(EnumType.ORDINAL)
     private Set<Tecnologias> suaTecnologia;
 
+    public Desenvolvedor(String nome, String experiencias, Usuario seuUsuario, String numeroTelefone, AreaAtuacao areaAtuacao, Senioridade devSenioridade, Set<Tecnologias> suaTecnologia) {
+        this.nome = nome;
+        this.experiencias = experiencias;
+        this.seuUsuario = seuUsuario;
+        this.numeroTelefone = numeroTelefone;
+        this.areaAtuacao = areaAtuacao;
+        this.devSenioridade = devSenioridade;
+        this.suaTecnologia = suaTecnologia;
+    }
+
+    @Deprecated
+    public Desenvolvedor() {
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getExperiencias() {
+        return experiencias;
+    }
+
+    public void setExperiencias(String experiencias) {
+        this.experiencias = experiencias;
+    }
+
+    public Usuario getSeuUsuario() {
+        return seuUsuario;
+    }
+
+    public void setSeuUsuario(Usuario seuUsuario) {
+        this.seuUsuario = seuUsuario;
+    }
+
+    public String getNumeroTelefone() {
+        return numeroTelefone;
+    }
+
+    public void setNumeroTelefone(String numeroTelefone) {
+        this.numeroTelefone = numeroTelefone;
+    }
+
+    public AreaAtuacao getAreaAtuacao() {
+        return areaAtuacao;
+    }
+
+    public void setAreaAtuacao(AreaAtuacao areaAtuacao) {
+        this.areaAtuacao = areaAtuacao;
+    }
+
+    public Senioridade getDevSenioridade() {
+        return devSenioridade;
+    }
+
+    public void setDevSenioridade(Senioridade devSenioridade) {
+        this.devSenioridade = devSenioridade;
+    }
+
+    public Set<Tecnologias> getSuaTecnologia() {
+        return suaTecnologia;
+    }
+
+    public void setSuaTecnologia(Set<Tecnologias> suaTecnologia) {
+        this.suaTecnologia = suaTecnologia;
+    }
 }
