829baef65ca7b99fd0d42d13624eee686481890e
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-03-24 02:22:02 -0300

    Finishing a populateSpy method on HomeController

--- src/main/java/br/com/pessoal/portifolio/controller/HomeController.java
+++ src/main/java/br/com/pessoal/portifolio/controller/HomeController.java
@@ -1,15 +1,15 @@
 package br.com.pessoal.portifolio.controller;
 
 
-import br.com.pessoal.portifolio.model.Desenvolvedor;
-import br.com.pessoal.portifolio.model.Profile;
-import br.com.pessoal.portifolio.model.Usuario;
+import br.com.pessoal.portifolio.model.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.List;
 
 @RestController
@@ -18,6 +18,7 @@ public class HomeController {
 
     @GetMapping
     public ResponseEntity<String> boasVindas(){
+        populateSpry();
         return ResponseEntity.ok("BOAS VINDAS AO SITE DE PORTIFÓLIOS ONLINE");
     }
 
@@ -34,6 +35,12 @@ public class HomeController {
         usuarios.add(new Usuario("mileide_dev@email.com", "$2y$12$ox8SpbwupjeTewN6xg5o/e.JqzJ3aJdBLoGkWJb7eCzi1QecPDgIa", profiles.get(0)));
         usuarios.add(new Usuario("anderson_galdino@email.com", "$2y$12$k1izJKsROxfRwGAt2tHLR.96eAOMtHR5Y4hEQDEJIYx9yKV1jsRD.", profiles.get(1)));
 
+        desenvolvedores.add(new Desenvolvedor("Felipe", "Algumas experiencias", usuarios.get(0),"11912345678",
+                AreaAtuacao.BACKEND, Senioridade.JUNIOR, EnumSet.of(Tecnologias.C,Tecnologias.JAVA,Tecnologias.HTML,Tecnologias.CSS)));
+        desenvolvedores.add(new Desenvolvedor("Mileide", "Varias experiencias", usuarios.get(1),"33985246528",
+                AreaAtuacao.FRONTEND, Senioridade.SENIOR, EnumSet.of(Tecnologias.JAVASCRIPT,Tecnologias.HTML,Tecnologias.CSS,Tecnologias.PYTHON)));
+        desenvolvedores.add(new Desenvolvedor("Anderson", "Uma porrada de experiencia", usuarios.get(2),"55845627142",
+                AreaAtuacao.FULLSTACK, Senioridade.PLENO, EnumSet.of(Tecnologias.CSHARP,Tecnologias.JAVASCRIPT,Tecnologias.HTML,Tecnologias.CSS,Tecnologias.PHP, Tecnologias.PYTHON)));
 
     }
 
