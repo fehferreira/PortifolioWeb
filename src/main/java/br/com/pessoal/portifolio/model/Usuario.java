6cf41ee4ed410a1d4232636e017748fbf4390ffb
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-03-24 01:12:47 -0300

    Refactor Usuario entity to be use in authorization part of project

--- src/main/java/br/com/pessoal/portifolio/model/Usuario.java
+++ src/main/java/br/com/pessoal/portifolio/model/Usuario.java
@@ -1,22 +1,61 @@
 package br.com.pessoal.portifolio.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.util.Arrays;
+import java.util.Collection;
 
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     private String username;
     private String password;
+
+    @OneToOne
     private Profile profile;
+
+    public Usuario(String username, String password, Profile profile) {
+        this.username = username;
+        this.password = password;
+        this.profile = profile;
+    }
 
     public String getUsername() {
         return username;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(this.profile);
     }
 
     public String getPassword() {
