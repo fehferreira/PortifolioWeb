b639e44f5462d2a4ad8311bcf8b43900c11df173
Felipe <felipe.ferreira1003@gmail.com>
2021-03-28 19:14:43 -0300

    Refact DesenvolvedorAtualizadoForm class to a better type of data

--- src/main/java/br/com/pessoal/portifolio/controller/form/DesenvolvedorAtualizadoForm.java
+++ src/main/java/br/com/pessoal/portifolio/controller/form/DesenvolvedorAtualizadoForm.java
@@ -1,18 +1,25 @@
 package br.com.pessoal.portifolio.controller.form;
 
-import br.com.pessoal.portifolio.model.Usuario;
+import br.com.pessoal.portifolio.model.*;
+import br.com.pessoal.portifolio.repository.UsuarioRepository;
+
+import java.util.Set;
 
 public class DesenvolvedorAtualizadoForm {
 
     private Long id;
     private String nome;
-    private String sobreNome;
-    private String cpf;
-    private String endereco;
-    private String cargo;
-
+    private String experiencias;
     private Usuario seuUsuario;
+    private String telefone;
+    private AreaAtuacao suaArea;
+    private Senioridade suaSenioridade;
+    private Set<Tecnologias> suaTecnologia;
 
+    public Desenvolvedor converter(UsuarioRepository usuarioRepository) {
+        return new Desenvolvedor(this.nome,this.experiencias,usuarioRepository.findByUsername(this.seuUsuario.getUsername()),
+                this.telefone,this.suaArea,this.suaSenioridade,this.suaTecnologia);
+    }
 
     public Long getId() {
         return id;
@@ -30,36 +37,12 @@ public class DesenvolvedorAtualizadoForm {
         this.nome = nome;
     }
 
-    public String getSobreNome() {
-        return sobreNome;
+    public String getExperiencias() {
+        return experiencias;
     }
 
-    public void setSobreNome(String sobreNome) {
-        this.sobreNome = sobreNome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
+    public void setExperiencias(String experiencias) {
+        this.experiencias = experiencias;
     }
 
     public Usuario getSeuUsuario() {
@@ -68,5 +51,37 @@ public class DesenvolvedorAtualizadoForm {
 
     public void setSeuUsuario(Usuario seuUsuario) {
         this.seuUsuario = seuUsuario;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public AreaAtuacao getSuaArea() {
+        return suaArea;
+    }
+
+    public void setSuaArea(AreaAtuacao suaArea) {
+        this.suaArea = suaArea;
+    }
+
+    public Senioridade getSuaSenioridade() {
+        return suaSenioridade;
+    }
+
+    public void setSuaSenioridade(Senioridade suaSenioridade) {
+        this.suaSenioridade = suaSenioridade;
+    }
+
+    public Set<Tecnologias> getSuaTecnologia() {
+        return suaTecnologia;
+    }
+
+    public void setSuaTecnologia(Set<Tecnologias> suaTecnologia) {
+        this.suaTecnologia = suaTecnologia;
     }
 }
