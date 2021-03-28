f2019b924ac3d3091b568b31d6c81fc8ba2eaa21
Felipe <felipe.ferreira1003@gmail.com>
2021-03-28 20:25:48 -0300

    Desconfigure all users and profiles config and remove dependence of security spring.

--- src/main/java/br/com/pessoal/portifolio/model/Profile.java
+++ src/main/java/br/com/pessoal/portifolio/model/Profile.java
@@ -1,14 +1,12 @@
 package br.com.pessoal.portifolio.model;
 
-import org.springframework.security.core.GrantedAuthority;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Profile implements GrantedAuthority{
+public class Profile{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,8 +30,4 @@ public class Profile implements GrantedAuthority{
         this.type = type;
     }
 
-    @Override
-    public String getAuthority() {
-        return this.type;
-    }
 }
--- src/main/java/br/com/pessoal/portifolio/model/Usuario.java
+++ src/main/java/br/com/pessoal/portifolio/model/Usuario.java
@@ -1,16 +1,11 @@
 package br.com.pessoal.portifolio.model;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-import java.util.Arrays;
-import java.util.Collection;
 
 @Entity
-public class Usuario implements UserDetails {
+public class Usuario{
 
     @Id
     private String username;
@@ -33,33 +28,8 @@ public class Usuario implements UserDetails {
         return username;
     }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(this.profile);
     }
 
     public String getPassword() {
